b38a9d9aaa42ced4a032acece13ac5188c1f6aaa
Chris Lewis <clewis@novus.com>
2012-07-28 18:29:58 -0400

    in-plan notes

--- src/main/scala/ProblemsPlan.scala
+++ src/main/scala/ProblemsPlan.scala
@@ -3,6 +3,14 @@ package com.novus.tradesight
 import unfiltered.request._
 import unfiltered.response._
 
+/*
+ * Notes
+ * 
+ * ReturnsIndex is defined in the package object as:
+ *   type ReturnsIndex = String => Option[SortedMap[DateTime, Double]]
+ *
+ * Use this to access the return streams of the stocks.
+ */
 object ProblemsPlan {
   def apply(returnStreams: ReturnsIndex) =
     unfiltered.filter.Planify {
